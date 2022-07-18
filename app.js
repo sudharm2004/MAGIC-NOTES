b2db82ca52e35147f50606492981d918aa9a12b9
sudharm jadhav <93636952+sudharm2004@users.noreply.github.com>
2022-07-18 22:13:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log('welcome to notes app');
-window.onload = showNotes();
+window.onload = showNotes(), onload_stars();
 
 //GRABBING ADD NOTES WALA BUTTON U
 let addBtn = document.getElementById('addBtn');
@@ -31,6 +31,7 @@ addBtn.addEventListener('click', function () {
     addTxt.value = "";
     addTitle.value = "";
     showNotes();
+    onload_stars();
 });
 
 
@@ -46,14 +47,30 @@ function showNotes() {
         notesobj = JSON.parse(notes);
     }
 
+
+    //SAME LOGIC AS NOTES
+    let notesTitle = localStorage.getItem('notesTitle');
+    let notesTitleObj;
+    if (notesTitle == null) {
+        notesTitleObj = []
+    }
+    else {
+        notesTitleObj = JSON.parse(notesTitle);
+    }
+
     let html = "";
     notesobj.forEach(function (element, index) {
         html += `
 <div class="notesCard my-2 mx-2 card" style="width: 18rem;">
     <div class="card-body">
-      <h4 class="card-title noteTitle" contenteditable="true">${element.title}</span></h4><hr>
+    <div style="display:flex; justify-content:space-between;">
+        <h4 class="card-title noteTitle" contenteditable="true">${element.title}</h4><span class="fa fa-star checked fa-lg star-icon" onclick="star(this.id)" id="${index * 100 + 1}"></span>
+    </div>
+    <hr>
       <p class="card-text" contenteditable="true">${element.notes}</p>
       <a id="${index}" class="btn btn-primary" onclick="deleteNote(this.id)">Delete Note</a>  
+
+
     </div>
   </div>
 `;
@@ -68,6 +85,7 @@ function showNotes() {
     }
 }
 
+
 //FUNCTION TO DELETE THE NODE
 function deleteNote(index) {
     //CODE TO REMOVE THE NOTE(DESIRED NOTE) TEXT FROM LOCAL STORAGE
@@ -75,9 +93,80 @@ function deleteNote(index) {
     let notesobj = JSON.parse(notes);
     notesobj.splice(index, 1);
     localStorage.setItem('notes', JSON.stringify(notesobj));
+    //CODE TO REMOVE THE VALUE OF THE STAR STORED IN LOCAL STORAGE
+    let stars = localStorage.getItem('stars');
+    let starsobj = JSON.parse(stars);
+    for (var i = 0; i < starsobj.length; i++) {
+        if (starsobj[i] == index * 100 + 1) {
+            starsobj.splice(i, 1);
+        }
+    }
+    //CODE TO CHANGE THE VALUE OF STARS BY -100 THAT ARE GREATER THAN CURRENT VALUE TO MAKE IT SAME AS BEFORE DELETION
+    for (var i = 0; i < starsobj.length; i++) {
+        if (starsobj[i] > index * 100 + 1) {
+            starsobj[i] = starsobj[i] - 100;
+        }
+    }
+    console.log(starsobj);
+    localStorage.setItem('stars', JSON.stringify(starsobj));
     showNotes();
+    onload_stars();
 }
 
+//FUNCTION TO CHANGE THE COLOR OF STAR AND STORING ITS ID IN LOCAL STORAGE
+function star(element) {
+    let a = document.getElementById(element);
+    let stars = localStorage.getItem('stars');
+    let starsobj;
+
+    if (stars == null) {
+        starsobj = []
+    }
+    else {
+        starsobj = JSON.parse(stars);
+    }
+
+    if (a.style.color == "lightgray") {
+        a.style.color = "#0800ff";
+        starsobj.push(element);
+        localStorage.setItem('stars', JSON.stringify(starsobj));
+    } else {
+        a.style.color = "lightgray";
+        for (var i = 0; i < starsobj.length; i++) {
+
+            if (starsobj[i] === element) {
+                starsobj.splice(i, 1);
+            }
+
+        }
+        localStorage.setItem('stars', JSON.stringify(starsobj));
+    }
+
+}
+
+//FUNCTION TO SHOW THE STARS THAT ARE STORED IN LOCAL STORAGE WHEN THE WEBSITE IS RELOADED OR WHEN WE ADD A NOTE(AS SHOWNOTES FUNCTION IS RUNNS WHEN WE ADD A NOTE)
+function onload_stars() {
+    let stars = localStorage.getItem('stars');
+    let starsobj;
+    if (stars == null) {
+        starsobj = [];
+    }
+    else {
+        starsobj = JSON.parse(stars);
+    }
+    if (starsobj.length == 0) {
+        console.log("nothing to show");
+    }
+    else {
+        Array.from(JSON.parse(localStorage.getItem('stars'))).forEach(function (element) {
+            // console.log(element);
+            let a = document.getElementById(element);
+            a.style.color = "#0800ff";
+
+        });
+    }
+
+}
 
 //CODE TO FILTER THE SEARCHED NOTE
 let search = document.getElementById('searchText');
